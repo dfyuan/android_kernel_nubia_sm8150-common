09e189e4c22bc4b06def3acb61fb38f5fcf4747d
Cyborg2017 <wangyang0987654@gmail.com>
2020-07-25 17:11:22 +0800

    drivers: power: Remove redefined CONFIG_NUBIA_CHARGE_FEATURE
    (cherry picked from commit 89f0282643560f010d4e162c461d2203fe162e3c)
    (cherry picked from commit 3e0f847bf87b51a1b97f758a88e8dd16c0cce7b1)

--- drivers/power/supply/qcom/smb5-lib.h
+++ drivers/power/supply/qcom/smb5-lib.h
@@ -90,13 +90,6 @@ enum print_reason {
 #define DOCK_VOTER			"DOCK_VOTER"
 #endif
 
-#if defined(CONFIG_NUBIA_CHARGE_FEATURE)
-#define USER_STEP_CHG			"USER_STEP_CHG"
-#define USER_JEITA_FV_CHG			"USER_JEITA_FV_CHG"
-#define USER_ADAPTER_CHG			"USER_ADAPTER_CHG"
-#define USER_LCD_CHG			"USER_LCD_CHG"
-#endif
-
 #define BOOST_BACK_STORM_COUNT	3
 #define WEAK_CHG_STORM_COUNT	8
 
@@ -538,25 +531,6 @@ struct smb_charger {
 	int			last_cc_soc;
 	int			usbin_forced_max_uv;
 	int			init_thermal_ua;
-
-
-#if defined(CONFIG_NUBIA_CHARGE_FEATURE)
-	bool			step_chg_base_soc;
-	int			step_chg_base_soc_value;
-	int			step_chg_base_soc_current;
-	struct delayed_work	step_charge_check_work;
-	bool			step_chg_vote_flag;
-	int			jeita_warm_stop_chg_soc;
-	
-	bool			lcd_on_limit_enable;
-	int			lcd_on_limit_temp;
-	int			lcd_on_limit_fcc;
-	int			lcd_on;
-	#if defined(CONFIG_FB)
-	struct notifier_block 	fb_notifier;
-	#endif
-#endif
-
 
 #if defined(CONFIG_NUBIA_CHARGE_FEATURE)
 	bool			step_chg_base_soc;
